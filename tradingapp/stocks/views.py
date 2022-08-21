0ba622ec71b90caa72f5efec36d3558540336a69
Christouccllo1 <corji1@hotmail.com>
2022-08-20 22:01:06 -0400

    added quanity to portfolio

--- tradingapp/stocks/views.py
+++ tradingapp/stocks/views.py
@@ -1,27 +1,37 @@
 from pyexpat.errors import messages
-from tkinter import E
+
 from django.shortcuts import render, redirect
 import requests, json 
 from .forms import StockForm
 from django.contrib import messages
 from .models import Stock 
 # Create your views here.
-def home(request):
+
     #pk_4c68895f25a94a97ba943173f0aa9638
-    return render(request,'home.html',{})
+    
 
 def home(request):
+    if(request.method == 'POST'):
+        ticker = request.POST['ticker']
+
+        api_req = requests.get("https://cloud.iexapis.com/stable/stock/"+ ticker + "/quote?token=pk_4c68895f25a94a97ba943173f0aa9638");
+        try:
+            res = json.loads(api_req.content)
+        except Exception as e:
+            res = "Error"
+        return render(request, 'ticker.html', {'api':res})
+    else:
     
-    stocks = []
-    api_req = requests.get(
-        "https://cloud.iexapis.com/stable/stock/market/list/mostactive?token=pk_4c68895f25a94a97ba943173f0aa9638")
-    try:
-        res = json.loads(api_req.content)
-        stocks = res
-    except Exception as e:
-        res = "Error"
-    
-    return render(request,'home.html',{"api": stocks})
+        stocks = []
+        api_req = requests.get(
+            "https://cloud.iexapis.com/stable/stock/market/list/mostactive?token=pk_4c68895f25a94a97ba943173f0aa9638")
+        try:
+            res = json.loads(api_req.content)
+            stocks = res
+        except Exception as e:
+            res = "Error"
+        
+        return render(request,'home.html',{"api": stocks})
 
 def add_stock(request):
     if request.method=="POST":
@@ -35,12 +45,22 @@ def add_stock(request):
 def portfolio(request):
     ticker=Stock.objects.all()
     stocks=[]
+    ticks_count = {}
     for tick in ticker:
-        api_req=requests.get("https://cloud.iexapis.com/stable/stock/"+ str(tick) + "/quote?token=pk_4c68895f25a94a97ba943173f0aa9638");
+        if(str(tick) in ticks_count.keys()):
+            ticks_count[str(tick)] = [ticks_count[str(tick)][0] + 1 , ticks_count[str(tick)][1]]
+        else:
+            ticks_count.update({str(tick): [1, tick.id]})
+            
+    
+    for t,c in ticks_count.items():
+        
+            
+        api_req=requests.get("https://cloud.iexapis.com/stable/stock/"+ str(t) + "/quote?token=pk_4c68895f25a94a97ba943173f0aa9638");
         try:
             dict={}
             res=json.loads(api_req.content)
-            dict.update({"id":tick.id,"ticker":res})
+            dict.update({"id":c[1],"ticker":res, "quantity": c[0]})
             stocks.append(dict)
         except Exception as e:
             res = e
